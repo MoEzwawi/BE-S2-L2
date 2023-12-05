b406bd765f8a1b5b97e8ade68c87e9b5586f6846
MoEzwawi <mezwawi96@gmail.com>
2023-12-05 18:31:08 +0100

    adds roller coaster feature

--- src/es2/Main.java
+++ src/es2/Main.java
@@ -9,7 +9,10 @@ public class Main {
         List<Integer> randomIntegers = giveMeOrderedRandomList(numberOfElems);
         System.out.println("Ecco un lista ordinata di "+numberOfElems+" interi: " +randomIntegers.toString());
         List<Integer> upAndDown = montagnaRussa(randomIntegers);
-        System.out.println("Ecco la lista che sale e scende: "+upAndDown);
+        System.out.println("Quante discese ti vuoi fare sulla montagna russa?");
+        int num = Integer.parseInt(input.nextLine());
+        List<Integer> repeatedUpAndDown = duplicateAndMerge(upAndDown,num);
+        drawGraph(repeatedUpAndDown);
         int bool=0;
         System.out.println("Premi 1 per gli elementi in posizione dispari");
         System.out.println("Premi 2 per gli elementi in posizione pari");
@@ -32,7 +35,7 @@ public class Main {
         List<Integer> randomIntegers = new ArrayList<>();
         for(int i=0;i<n;i++){
             Random rndm = new Random();
-            randomIntegers.add(rndm.nextInt(101));
+            randomIntegers.add(rndm.nextInt(16));
         }
         Collections.sort(randomIntegers);
         return randomIntegers;
@@ -43,6 +46,7 @@ public class Main {
         rollerCoasterList.addAll(list);
         Collections.sort(list,Collections.reverseOrder());
         rollerCoasterList.addAll(list);
+        rollerCoasterList.remove(Math.floor((double) rollerCoasterList.size() /2));
         return rollerCoasterList;
     }
     private static void pariODispari(List<Integer> list, boolean b){
@@ -55,5 +59,22 @@ public class Main {
                 System.out.println(list.get(i)+" posizione= "+(i+1));
             }
         }
+    }
+    private static void drawGraph(List<Integer> list){
+        System.out.println("Ecco la montagna russa:");
+        for(int i=0;i<list.size();i++){
+            String str = "";
+            for(int j=0;j<list.get(i);j++){
+                str += "*";
+            }
+            System.out.println(str);
+        }
+    }
+    private static List duplicateAndMerge(List list,int n){
+        List l = new ArrayList<>();
+        for (int i=0; i<n;i++){
+            l.addAll(list);
+        }
+        return l;
     }
 }
